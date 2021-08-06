0a50fb824c180d01d2b7d1eb395ded6a43958d4e
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2021-08-06 11:45:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(380, 380)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
